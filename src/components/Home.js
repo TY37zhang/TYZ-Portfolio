6ffca953a4f8c2869aa9cd845e1ee90ef11c6bdb
TY37zhang <83420931+TY37zhang@users.noreply.github.com>
2023-06-01 05:41:41 -0700

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,65 @@
+import SectionContainer from "./SectionContainer";
+const socialIcon = [
+  {
+    id: 1,
+    iconName: "icon-instagram-1",
+    link: "https://www.instagram.com/aka_ty37/",
+  },
+  {
+    id: 2,
+    iconName: "icon-github-squared",
+    link: "https://github.com/TY37zhang",
+  },
+  {
+    id: 3,
+    iconName: "icon-linkedin-squared",
+    link: "https://www.linkedin.com/in/tianyinzhang37/",
+  },
+];
+const Home = () => {
+  return (
+    <SectionContainer name={"home"}>
+      <div className="container">
+        <div className="tokyo_tm_home w-full min-h-[100vh] clear-both flex items-center justify-center relative">
+          <div className="home_content flex items-center">
+            <div
+              className="avatar min-w-[300px] min-h-[300px] relative rounded-full"
+              data-type="wave"
+            >
+              {" "}
+              {/* data-type values are: "wave", "circle", "square"*/}
+              <div
+                className="image absolute inset-0 bg-no-repeat bg-center bg-cover"
+                data-img-url="assets/img/slider/1.jpg"
+              />
+            </div>
+            <div className="details ml-[80px]">
+              <h3 className="name text-[55px] font-extrabold uppercase mb-[14px]">
+                Tianyin <span>Zhang</span>
+              </h3>
+              <p className="job font-montserrat font-medium max-w-[450px] mb-[25px]">
+                I am a newly graduated student at New York University, majoring in Computer Science, with a minor in Web Programming and Applications.
+              </p>
+              <div className="social w-full float-left">
+                <ul className="m-0 list-none">
+                  {socialIcon.map((item) => (
+                    <li className="mr-[8px] inline-block" key={item.id}>
+                      <a
+                        className="text-black text-[20px] transition-all duration-300 hover:text-black"
+                        href={item.link}
+                        target="_blank"
+                      >
+                        <i className={item.iconName} />
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </SectionContainer>
+  );
+};
+export default Home;
