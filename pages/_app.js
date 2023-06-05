7d22f272b408ece8c2cdd8dc849e81f94a81a7eb
TY37zhang <83420931+TY37zhang@users.noreply.github.com>
2023-06-05 08:38:25 -0700

    feat: vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import TokyoState from "@/src/Context";
 import PreLoader from "@/src/layout/PreLoader";
 import "@/styles/globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
     <TokyoState>
       <PreLoader />
       <Component {...pageProps} />
+        <Analytics />
     </TokyoState>
   );
 }
