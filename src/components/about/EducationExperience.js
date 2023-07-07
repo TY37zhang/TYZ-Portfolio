002c65e4ba2281addadcb8179b11b5b1f4580799
TY37zhang <83420931+TY37zhang@users.noreply.github.com>
2023-07-06 22:02:18 -0700

    misc: update experience details

--- src/components/about/EducationExperience.js
+++ src/components/about/EducationExperience.js
@@ -1,109 +1,108 @@
 const experiencesData = [
-    
-    {
-        id: 1,
-        date: "Jan 2023 - May 2023",
-        designation: "Full Stack Web Developer",
-        company: "OpenEDU - MERN Mobile Web App - Course Project",
-    },
-    {
-        id: 2,
-        date: "Sep 2022 - Dec 2022",
-        designation: "Full Stack Web Developer",
-        company: "Plant Your Dreams - ReactJS Web App- Course Project",
-    },
-    {
-        id: 3,
-        date: "Oct 2021 - Current",
-        designation: "Founder",
-        company: "PEV NYC",
-    },
-    {
-        id:4,
-        date: "Mar 2018 - Dec 2019",
-        designation: "Founder",
-        company: "Online Discord Gaming Community"
-    },
+  {
+    id: 1,
+    date: "Jan 2023 - May 2023",
+    designation: "Full Stack Web Developer",
+    company: "OpenEDU - MERN Mobile Web App - School Project",
+  },
+  {
+    id: 2,
+    date: "Sep 2022 - Dec 2022",
+    designation: "Full Stack Web Developer",
+    company: "Plant Your Dreams - ReactJS Web App- School Project",
+  },
+  {
+    id: 3,
+    date: "Oct 2021 - Current",
+    designation: "Founder",
+    company: "PEV NYC",
+  },
+  {
+    id: 4,
+    date: "Mar 2018 - Dec 2019",
+    designation: "Founder",
+    company: "Online Discord Gaming Community",
+  },
 ];
 const educationData = [
-    {
-        id: 1,
-        date: "2019 - 2023",
-        degree: "Bachelor of Arts",
-        institute: "New York University",
-    },
+  {
+    id: 1,
+    date: "2019 - 2023",
+    degree: "Bachelor of Arts",
+    institute: "New York University",
+  },
 ];
 
 const EducationExperience = () => {
-    return (
-        <div className="tokyo_tm_resumebox w-full h-auto clear-both float-left bg-white py-[93px] px-0">
-            <div className="container">
-                <div className="in w-full h-auto clear-both float-left flex">
-                    <div className="left w-1/2 pr-[50px]">
-                        <div className="tokyo_section_title w-full h-auto clear-both float-left mb-[40px]">
-                            <h3 className="text-[20px] font-bold">Education</h3>
-                        </div>
-                        <div className="tokyo_tm_resume_list w-full h-auto clear-both float-left">
-                            <ul className="m-0 list-none relative inline-block pt-[10px]">
-                                {educationData.map((edu, i) => (
-                                    <li
-                                        className={`m-0 w-full float-left relative pl-[20px] ${
-                                            i == educationData.length - 1 ? "" : "pb-[45px]"
-                                        }`}
-                                        key={edu.id}
-                                    >
-                                        <div className="list_inner  w-full h-auto clear-both float-left relative flex">
-                                            <div className="time w-1/2 pr-[20px]">
+  return (
+    <div className="tokyo_tm_resumebox w-full h-auto clear-both float-left bg-white py-[93px] px-0">
+      <div className="container">
+        <div className="in w-full h-auto clear-both float-left flex">
+          <div className="left w-1/2 pr-[50px]">
+            <div className="tokyo_section_title w-full h-auto clear-both float-left mb-[40px]">
+              <h3 className="text-[20px] font-bold">Education</h3>
+            </div>
+            <div className="tokyo_tm_resume_list w-full h-auto clear-both float-left">
+              <ul className="m-0 list-none relative inline-block pt-[10px]">
+                {educationData.map((edu, i) => (
+                  <li
+                    className={`m-0 w-full float-left relative pl-[20px] ${
+                      i == educationData.length - 1 ? "" : "pb-[45px]"
+                    }`}
+                    key={edu.id}
+                  >
+                    <div className="list_inner  w-full h-auto clear-both float-left relative flex">
+                      <div className="time w-1/2 pr-[20px]">
                         <span className="inline-block py-[5px] px-[25px] bg-[rgba(0,0,0,.05)] rounded-[50px] text-[14px] whitespace-nowrap">
                           {edu.date}
                         </span>
-                                            </div>
-                                            <div className="place w-1/2 pl-[20px]">
-                                                <h3 className="text-[16px] mb-[2px] font-semibold">
-                                                    {edu.institute}
-                                                </h3>
-                                                <span className="text-[14px]">{edu.degree}</span>
-                                            </div>
-                                        </div>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
+                      </div>
+                      <div className="place w-1/2 pl-[20px]">
+                        <h3 className="text-[16px] mb-[2px] font-semibold">
+                          {edu.institute}
+                        </h3>
+                        <span className="text-[14px]">{edu.degree}</span>
+                      </div>
                     </div>
-                    <div className="right w-1/2 pl-[50px]">
-                        <div className="tokyo_section_title w-full h-auto clear-both float-left mb-[40px]">
-                            <h3 className="text-[20px] font-bold">Experience</h3>
-                        </div>
-                        <div className="tokyo_tm_resume_list w-full h-auto clear-both float-left">
-                            <ul className="m-0 list-none relative inline-block pt-[10px]">
-                                {experiencesData.map((exp, i) => (
-                                    <li
-                                        className={`m-0 w-full float-left relative pl-[20px] ${
-                                            i == experiencesData.length - 1 ? "" : "pb-[45px]"
-                                        }`}
-                                        key={exp.id}
-                                    >
-                                        <div className="list_inner  w-full h-auto clear-both float-left relative flex">
-                                            <div className="time w-1/2 pr-[20px]">
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className="right w-1/2 pl-[50px]">
+            <div className="tokyo_section_title w-full h-auto clear-both float-left mb-[40px]">
+              <h3 className="text-[20px] font-bold">Experience</h3>
+            </div>
+            <div className="tokyo_tm_resume_list w-full h-auto clear-both float-left">
+              <ul className="m-0 list-none relative inline-block pt-[10px]">
+                {experiencesData.map((exp, i) => (
+                  <li
+                    className={`m-0 w-full float-left relative pl-[20px] ${
+                      i == experiencesData.length - 1 ? "" : "pb-[45px]"
+                    }`}
+                    key={exp.id}
+                  >
+                    <div className="list_inner  w-full h-auto clear-both float-left relative flex">
+                      <div className="time w-1/2 pr-[20px]">
                         <span className="inline-block py-[5px] px-[25px] bg-[rgba(0,0,0,.05)] rounded-[50px] text-[14px] whitespace-nowrap">
                           {exp.date}
                         </span>
-                                            </div>
-                                            <div className="place w-1/2 pl-[20px]">
-                                                <h3 className="text-[16px] mb-[2px] font-semibold">
-                                                    {exp.company}
-                                                </h3>
-                                                <span className="text-[14px]">{exp.designation}</span>
-                                            </div>
-                                        </div>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
+                      </div>
+                      <div className="place w-1/2 pl-[20px]">
+                        <h3 className="text-[16px] mb-[2px] font-semibold">
+                          {exp.company}
+                        </h3>
+                        <span className="text-[14px]">{exp.designation}</span>
+                      </div>
                     </div>
-                </div>
+                  </li>
+                ))}
+              </ul>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 export default EducationExperience;
